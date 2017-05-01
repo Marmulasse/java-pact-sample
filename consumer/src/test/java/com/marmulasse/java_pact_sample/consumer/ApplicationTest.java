04e5758e7f8c37ed8ee0ef91ca7f3a1c54334ad6
dpanza <david.panza@gmail.com>
2017-05-01 23:52:48 +0200

    valid pact generation for HTTP call

--- consumer/src/test/java/com/marmulasse/java_pact_sample/consumer/ApplicationTest.java
+++ consumer/src/test/java/com/marmulasse/java_pact_sample/consumer/ApplicationTest.java
@@ -1,36 +1,52 @@
 package com.marmulasse.java_pact_sample.consumer;
 
 
-import au.com.dius.pact.consumer.ConsumerPactBuilder;
-import au.com.dius.pact.consumer.PactError;
-import au.com.dius.pact.consumer.TestRun;
-import au.com.dius.pact.consumer.VerificationResult;
+import au.com.dius.pact.consumer.*;
+import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
 import au.com.dius.pact.model.MockProviderConfig;
 import au.com.dius.pact.model.PactFragment;
+import org.assertj.core.api.Assertions;
+import org.junit.Rule;
 import org.junit.Test;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+
+import static org.junit.Assert.assertThat;
 
 /**
  * Created by dpanza on 15/04/17.
  */
 public class ApplicationTest {
 
-	@Test
-	public void testPact() {
-		PactFragment pactFragment = ConsumerPactBuilder
-				.consumer("Some Consumer")
-				.hasPactWith("Some Provider")
-				.uponReceiving("a request to say Hello")
-				.path("/hello")
-				.method("POST")
-				.body("{\"name\": \"harry\"}")
-				.willRespondWith()
-				.status(200)
-				.body("{\"hello\": \"harry\"}")
-				.toFragment();
-	}
+    @Rule
+    public PactProviderRule provider = new PactProviderRule("spring-boot-provider", "localhost", 8080, this);
 
+    @Pact(state = "default", provider = "spring-boot-provider", consumer = "spring-boot-consumer")
+    public PactFragment createFragment(PactDslWithProvider builder) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("content-type", "application/json");
+
+        return builder
+                .given("default")
+                .uponReceiving("Test Provider")
+                .path("/api/provider/sample")
+                .method("GET")
+                .willRespondWith()
+                .status(200)
+                .headers(headers)
+                .body("{\"label\": \"coucou\"}")
+                .toFragment();
+    }
+
+    @Test
+    @PactVerification("spring-boot-provider")
+    public void runTest() throws IOException {
+        final RestTemplate call = new RestTemplate();
+        final ResponseEntity<String> forEntity = call.getForEntity(provider.getConfig().url() + "/api/provider/sample", String.class);
+        Assertions.assertThat(forEntity.getBody()).isEqualTo("{\"label\": \"coucou\"}");
+    }
 }
--- provider/src/main/java/com/marmulase/java_pact_sample/provider/rest/ProviderController.java
+++ provider/src/main/java/com/marmulase/java_pact_sample/provider/rest/ProviderController.java
@@ -1,5 +1,7 @@
 package com.marmulase.java_pact_sample.provider.rest;
 
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ProviderController {
 
-	@RequestMapping(path = "/api/provider/sample", method = RequestMethod.GET)
-	public String get() {
-		return "coucou";
+	@RequestMapping(path = "/api/provider/sample", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity get() {
+		return ResponseEntity.ok(new SampleObject("coucou"));
+	}
+	
+	public class SampleObject {
+		private String label;
+
+		public SampleObject(String label) {
+			this.label = label;
+		}
+
+		public String getLabel() {
+			return label;
+		}
 	}
 }
